3fcf5ff11512268ded2cfc7585f405579917bce6
Jeferson 'Shin' Leite Borges <30536099+shinspiegel@users.noreply.github.com>
2023-09-05 07:47:16 +0000

    Updated tests

--- src/utils/camelize.ts
+++ src/utils/camelize.ts
@@ -1,2 +1,1 @@
-export const camelize = (string: string): string =>
-  string.trim().replace(/-./g, (match) => match.toUpperCase()[1]);
+export const camelize = (string: string): string => string.trim().replace(/-./g, (match) => match.toUpperCase()[1]);
